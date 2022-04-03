63a0da132b3fa6fc603b367173576c25d8fbe3b4
ericatsu <ekaatsu@st.ug.edu.gh>
2022-04-03 00:26:51 +0000

    components and container created

---
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Testimonial.scss';
+
+const Testimonial = () => {
+  return (
+    <div>Testimonal</div>
+  )
+}
+
+export default Testimonial
---
+++ frontend_react/src/container/index.js
@@ -0,0 +1,6 @@
+export { default as Header} from './Header/Header';
+export { default as About} from './About/About';
+export { default as Skills} from './Skills/Skills';
+export { default as Work} from './Work/Work';
+export { default as Testimonial } from './Testimonial/Testimonial'
+export { default as Footer} from './Footer/Footer';
