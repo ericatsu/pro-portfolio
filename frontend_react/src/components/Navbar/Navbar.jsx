4fb345927df38ba0847461c71a81819027269bc7
ericatsu <ekaatsu@st.ug.edu.gh>
2022-04-05 00:59:03 +0000

    navbar created,

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -5,10 +5,19 @@ import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    //Using BEM
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
         <img src={images.logo} alt='logo' />
       </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+           <div />
+           <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
