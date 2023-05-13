d606fcf5d0b2f858be7cb01cbb05615579193c44
ericatsu <ekaatsu@st.ug.edu.gh>
2023-05-13 19:24:30 +0000

    🚑

--- frontend_react/src/container/Hero/Hero.jsx
+++ frontend_react/src/container/Hero/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Micael</h1>
+            <h1 className="head-text">Lester</h1>
           </div>
         </div>
 
@@ -44,7 +44,7 @@ const Hero = () => (
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.profile} alt="profile_bg" />
+      <img src="{images.mobile}" alt="mobile_bg" />
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
